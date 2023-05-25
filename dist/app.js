3e7dbf626cc7f529f9c7a52f8d0bc8a6820224c8
Nahom-Derese <nahomderese@gmail.com>
2023-05-25 05:05:12 +0300

    🚀Initial Commit

---
+++ dist/app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+const app = express();
+const port = 3000;
+app.use(bodyParser.json());
+app.get('/', (req, res) => {
+    res.send('Hello Hello Hello Hello My World!');
+});
+'';
+app.get('/hi', (req, res) => {
+    res.send('Hello World!');
+});
+'';
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
+//# sourceMappingURL=app.js.map
